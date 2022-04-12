8ea9cec36021b7b71163b4ec6b1042c52a87bfcc
Wilson <wiston.98@hotmail.com>
2022-04-11 20:43:55 -0500

    cambio de fuente

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-const mediumBp = matchMedia("(min-width: 600px)");
+const mediumBp = matchMedia("(min-width: 600px) and (orientation: landscape)");
 const changeSize = (mql) => {
   mql.matches
     ? (document.body.style.background = "black")
     : (document.body.style.background = "orange");
 };
 
-mediumBp.addListener(changeSize);
-changeSize(mediumBp);
+addEventListener("resize", () => changeSize(mediumBp));
+addEventListener("DomContentLoaded", () => changeSize(mediumBp));
+
+///esto no es recomendable para aplicar media querys, siempre que se puedan hacer con CSS es lo mas reomendable
+//Solo se recomienda usar esto con javascrip cuando necesitamos agregarle o ponerle clases a algunos elementos
+//del DOM en algunos tipos de pantalla
